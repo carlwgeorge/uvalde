a995f72dd055d48169de021574a50c93e969eca5
Carl George <carl@george.computer>
2019-03-24 18:34:13 -0500

    Add RepoConfig class to access settings for a repo

--- source/uvalde/config.py
+++ source/uvalde/config.py
@@ -2,6 +2,40 @@ import configparser
 import pathlib
 
 import appdirs
+import click
+
+
+class RepoConfig:
+    """Object to access settings for a repo."""
+
+    __slots__ = ['section']
+
+    def __init__(self, section):
+        self.section = section
+
+    def __str__(self):
+        return self.section.name
+
+    @property
+    def base(self):
+        """Look up base setting of repo."""
+
+        try:
+            return pathlib.Path(self.section['base'])
+        except KeyError:
+            raise SystemExit(click.style('missing parameter base', fg='red'))
+
+    @property
+    def architectures(self):
+        """Look up architectures setting of repo."""
+
+        try:
+            return [
+                architecture.strip()
+                for architecture in self.section['architectures'].split(',')
+            ]
+        except KeyError:
+            raise SystemExit(click.style('missing parameter architectures', fg='red'))
 
 
 class Config:
@@ -15,6 +49,18 @@ class Config:
             config_dir.mkdir(parents=True)
         self.file = config_dir / 'repos.ini'
         self.parser = configparser.ConfigParser()
+
+    def __iter__(self):
+        for name in self.parser.sections():
+            yield RepoConfig(self.parser[name])
+
+    def __getitem__(self, repo):
+        """Look up config section of repo."""
+
+        try:
+            return RepoConfig(self.parser[repo])
+        except KeyError:
+            raise SystemExit(click.style(f'{repo} repo not configured', fg='red'))
 
     def load(self):
         """Load repo configurations from config file."""
