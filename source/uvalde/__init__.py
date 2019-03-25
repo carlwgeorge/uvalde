1fa5a1bdc2969954d16dbcde87b139f45fbd1a5c
Carl George <carl@george.computer>
2019-03-24 19:03:10 -0500

    Add 'list repos' subcommand

--- source/uvalde/__init__.py
+++ source/uvalde/__init__.py
@@ -1,6 +1,18 @@
 import click
 
+from uvalde.commands.list_repos import list_repos
+
 
 @click.group()
 def main():
     """Yum repository management tool."""
+
+
+@click.group('list')
+def list_():
+    """List subcommands."""
+
+
+list_.add_command(list_repos)
+
+main.add_command(list_)
---
+++ source/uvalde/commands/list_repos.py
@@ -0,0 +1,12 @@
+import click
+
+from uvalde.config import config
+
+
+@click.command('repos')
+def list_repos():
+    """List configured repos"""
+
+    config.load()
+    for repo in config:
+        click.secho(f'{repo}', fg='cyan')
