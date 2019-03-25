23c093c124095fbfbb2795f47558c36f4ee804c6
Carl George <carl@george.computer>
2019-03-25 13:09:02 -0500

    Fix docstring typo

--- source/uvalde/commands/list_repos.py
+++ source/uvalde/commands/list_repos.py
@@ -5,7 +5,7 @@ from uvalde.config import config
 
 @click.command('repos')
 def list_repos():
-    """List configured repos"""
+    """List configured repos."""
 
     config.load()
     for repo in config:
