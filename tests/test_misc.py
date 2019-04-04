f7f1a3daf95bc3ec20af289de47192b297b6691e
Carl George <carl@george.computer>
2019-04-04 00:37:50 -0500

    Add test for repo not being configured

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -10,3 +10,12 @@ def test_no_config(tmp_path):
 
     assert f'{tmp_path}/.config/uvalde/repos.ini: does not exist' in result.output
     assert result.exit_code == 1
+
+
+def test_no_repo(tmp_config):
+    runner = click.testing.CliRunner()
+
+    result = runner.invoke(uvalde.main, ['list', 'nvrs', 'repo0'])
+
+    assert f'repo0: repo not configured' in result.output
+    assert result.exit_code == 1
