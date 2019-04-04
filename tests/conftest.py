d9bd6a747d563cb594f5e4400721334d3fa72d63
Carl George <carl@george.computer>
2019-04-04 11:06:17 -0500

    Add tests for missing parameters

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,6 +21,22 @@ def tmp_config(tmp_path):
     write_config(tmp_path, parser)
 
 
+@pytest.fixture
+def tmp_config_missing_base(tmp_path):
+    parser = configparser.ConfigParser()
+    parser.add_section('repo1')
+    parser['repo1']['architectures'] = 'i686, x86_64'
+    write_config(tmp_path, parser)
+
+
+@pytest.fixture
+def tmp_config_missing_architectures(tmp_path):
+    parser = configparser.ConfigParser()
+    parser.add_section('repo1')
+    parser['repo1']['base'] = str(tmp_path / 'repo1')
+    write_config(tmp_path, parser)
+
+
 @pytest.fixture(autouse=True)
 def tmp_env(monkeypatch, tmp_path):
     # isolate config and database files to the testing directory
--- tests/test_misc.py
+++ tests/test_misc.py
@@ -19,3 +19,21 @@ def test_no_repo(tmp_config):
 
     assert f'repo0: repo not configured' in result.output
     assert result.exit_code == 1
+
+
+def test_missing_base(tmp_config_missing_base):
+    runner = click.testing.CliRunner()
+
+    result = runner.invoke(uvalde.main, ['config', 'show', 'repo1'])
+
+    assert f'repo1: missing parameter base' in result.output
+    assert result.exit_code == 1
+
+
+def test_missing_architectures(tmp_config_missing_architectures):
+    runner = click.testing.CliRunner()
+
+    result = runner.invoke(uvalde.main, ['config', 'show', 'repo1'])
+
+    assert f'repo1: missing parameter architectures' in result.output
+    assert result.exit_code == 1
