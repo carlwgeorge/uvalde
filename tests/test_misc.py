fbe0ff62ddb974b3dcb6bf861b43399ac5ee0685
Carl George <carl@george.computer>
2019-04-04 13:57:51 -0500

    Test a few edge cases for safe_check

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -16,3 +16,34 @@ def test_createrepo(tmp_path, real_path):
     else:
         with pytest.raises(FileExistsError, match='No such directory'):
             uvalde.repodata.createrepo(target)
+
+
+@pytest.mark.parametrize(
+    'start_exists,end_exists',
+    [
+        (False, False),
+        (True, True),
+        (True, False),
+    ]
+)
+def test_safe_check(tmp_path, start_exists, end_exists):
+    start = tmp_path / 'a'
+    end = tmp_path / 'b'
+
+    if start_exists:
+        start.touch()
+    if end_exists:
+        end.touch()
+
+    if not start_exists:
+        with pytest.raises(SystemExit, match='a does not exist'):
+            uvalde.transfer.safe_check(start, end)
+
+    if end_exists:
+        with pytest.raises(SystemExit, match='b already exists'):
+            uvalde.transfer.safe_check(start, end)
+
+    if start_exists and not end_exists:
+        uvalde.transfer.safe_check(start, end)
+        assert end.parent.exists()
+        assert end.parent.is_dir()
