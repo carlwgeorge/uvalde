b6b0f096c26fb1a8067919c385dd35e7b8152c62
Carl George <carl@george.computer>
2019-04-04 11:01:45 -0500

    Use `config show` to test these edge cases

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -6,7 +6,7 @@ import uvalde
 def test_no_config(tmp_path):
     runner = click.testing.CliRunner()
 
-    result = runner.invoke(uvalde.main, ['list', 'repos'])
+    result = runner.invoke(uvalde.main, ['config', 'show', 'repo0'])
 
     assert f'{tmp_path}/.config/uvalde/repos.ini: does not exist' in result.output
     assert result.exit_code == 1
@@ -15,7 +15,7 @@ def test_no_config(tmp_path):
 def test_no_repo(tmp_config):
     runner = click.testing.CliRunner()
 
-    result = runner.invoke(uvalde.main, ['list', 'nvrs', 'repo0'])
+    result = runner.invoke(uvalde.main, ['config', 'show', 'repo0'])
 
     assert f'repo0: repo not configured' in result.output
     assert result.exit_code == 1
