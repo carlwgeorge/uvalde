114432ef22acce70368b79da9e05303f26b78ee4
Carl George <carl@george.computer>
2019-04-03 19:31:04 -0500

    Add test for `list all`

--- tests/test_list.py
+++ tests/test_list.py
@@ -6,6 +6,25 @@ import click.testing
 import uvalde
 
 
+def test_list_all(tmp_path):
+    runner = click.testing.CliRunner()
+
+    # import rpms so output is meaningful
+    args = ['import', '--keep-original', 'repo1']
+    test_data = pathlib.Path('tests/data')
+    args.extend(map(str, test_data.glob('*.rpm')))
+    runner.invoke(uvalde.main, args)
+
+    result = runner.invoke(uvalde.main, ['list', 'all'])
+
+    assert result.exit_code == 0
+    assert result.output == textwrap.dedent('''\
+        repo1
+          cello-1.0-1
+        repo2
+    ''')
+
+
 def test_list_nvrs(tmp_path):
     runner = click.testing.CliRunner()
 
