ed6405925336f45f7079150646afec80fccdb7de
Carl George <carl@george.computer>
2019-04-11 20:35:55 -0500

    Test that empty parent directories are removed

--- tests/test_move.py
+++ tests/test_move.py
@@ -34,6 +34,7 @@ def test_move(tmp_path, tmp_config):
     repo = repomd.load(f'file://{tmp_path}/repo1/src')
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/src/packages/c/cello-1.0-1.src.rpm').exists()
+    assert not (tmp_path / 'repo1/src/packages/c').exists()
 
     repo = repomd.load(f'file://{tmp_path}/repo2/src')
     assert [p.nevra for p in repo] == ['cello-1.0-1.src']
@@ -44,6 +45,7 @@ def test_move(tmp_path, tmp_config):
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/i686/packages/c/cello-1.0-1.i686.rpm').exists()
     assert not (tmp_path / 'repo1/i686/packages/c/cello-extra-1.0-1.noarch.rpm').exists()
+    assert not (tmp_path / 'repo1/i686/packages/c').exists()
 
     repo = repomd.load(f'file://{tmp_path}/repo2/i686')
     assert [p.nevra for p in repo] == ['cello-1.0-1.i686', 'cello-extra-1.0-1.noarch']
@@ -55,6 +57,7 @@ def test_move(tmp_path, tmp_config):
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/i686/debug/packages/c/cello-debuginfo-1.0-1.i686.rpm').exists()
     assert not (tmp_path / 'repo1/i686/debug/packages/c/cello-debugsource-1.0-1.i686.rpm').exists()
+    assert not (tmp_path / 'repo1/i686/debug/packages/c').exists()
 
     repo = repomd.load(f'file://{tmp_path}/repo2/i686/debug')
     assert [p.nevra for p in repo] == ['cello-debuginfo-1.0-1.i686', 'cello-debugsource-1.0-1.i686']
@@ -66,6 +69,7 @@ def test_move(tmp_path, tmp_config):
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/x86_64/packages/c/cello-1.0-1.x86_64.rpm').exists()
     assert not (tmp_path / 'repo1/x86_64/packages/c/cello-extra-1.0-1.noarch.rpm').exists()
+    assert not (tmp_path / 'repo1/x86_64/packages/c').exists()
 
     repo = repomd.load(f'file://{tmp_path}/repo2/x86_64')
     assert [p.nevra for p in repo] == ['cello-1.0-1.x86_64', 'cello-extra-1.0-1.noarch']
@@ -77,6 +81,7 @@ def test_move(tmp_path, tmp_config):
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/x86_64/debug/packages/c/cello-debuginfo-1.0-1.x86_64.rpm').exists()
     assert not (tmp_path / 'repo1/x86_64/debug/packages/c/cello-debugsource-1.0-1.x86_64.rpm').exists()
+    assert not (tmp_path / 'repo1/x86_64/debug/packages/c').exists()
 
     repo = repomd.load(f'file://{tmp_path}/repo2/x86_64/debug')
     assert [p.nevra for p in repo] == ['cello-debuginfo-1.0-1.x86_64', 'cello-debugsource-1.0-1.x86_64']
--- tests/test_remove.py
+++ tests/test_remove.py
@@ -35,27 +35,32 @@ def test_remove(tmp_path, tmp_config):
     repo = repomd.load(f'file://{tmp_path}/repo1/src')
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/src/packages/c/cello-1.0-1.src.rpm').exists()
+    assert not (tmp_path / 'repo1/src/packages/c').exists()
 
     # i686
     repo = repomd.load(f'file://{tmp_path}/repo1/i686')
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/i686/packages/c/cello-1.0-1.i686.rpm').exists()
     assert not (tmp_path / 'repo1/i686/packages/c/cello-extra-1.0-1.noarch.rpm').exists()
+    assert not (tmp_path / 'repo1/i686/packages/c').exists()
 
     # i686 debug
     repo = repomd.load(f'file://{tmp_path}/repo1/i686/debug')
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/i686/debug/packages/c/cello-debuginfo-1.0-1.i686.rpm').exists()
     assert not (tmp_path / 'repo1/i686/debug/packages/c/cello-debugsource-1.0-1.i686.rpm').exists()
+    assert not (tmp_path / 'repo1/i686/debug/packages/c').exists()
 
     # x86_64
     repo = repomd.load(f'file://{tmp_path}/repo1/x86_64')
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/x86_64/packages/c/cello-1.0-1.x86_64.rpm').exists()
     assert not (tmp_path / 'repo1/x86_64/packages/c/cello-extra-1.0-1.noarch.rpm').exists()
+    assert not (tmp_path / 'repo1/x86_64/packages/c').exists()
 
     # x86_64 debug
     repo = repomd.load(f'file://{tmp_path}/repo1/x86_64/debug')
     assert len(repo) == 0
     assert not (tmp_path / 'repo1/x86_64/debug/packages/c/cello-debuginfo-1.0-1.x86_64.rpm').exists()
     assert not (tmp_path / 'repo1/x86_64/debug/packages/c/cello-debugsource-1.0-1.x86_64.rpm').exists()
+    assert not (tmp_path / 'repo1/x86_64/debug/packages/c').exists()
