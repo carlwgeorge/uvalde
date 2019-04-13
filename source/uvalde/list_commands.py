1b541dbc1b1da610403d141aa496bb762ff6d361
Carl George <carl@george.computer>
2019-04-13 11:28:04 -0500

    Add name flag to filter list output

--- source/uvalde/list_commands.py
+++ source/uvalde/list_commands.py
@@ -4,7 +4,8 @@ from uvalde.configuration import load_config
 
 
 @click.command('all')
-def list_all():
+@click.option('-n', '--name', 'names', multiple=True, help='Limit output by package name.')
+def list_all(names):
     """List configured repos and NVRs in each."""
 
     config = load_config()
@@ -13,19 +14,24 @@ def list_all():
         srcpkgdir = repo.base / 'src' / 'packages'
         for srpm in srcpkgdir.glob('**/*.rpm'):
             nvr = srpm.name.replace('.src.rpm', '')
-            click.echo(f'  {nvr}')
+            name, _, _ = nvr.rsplit('-', maxsplit=2)
+            if not names or name in names:
+                click.echo(f'  {nvr}')
 
 
 @click.command('nvrs')
+@click.option('-n', '--name', 'names', multiple=True, help='Limit output by package name.')
 @click.argument('repo')
-def list_nvrs(repo):
+def list_nvrs(names, repo):
     """List RPM NVRs in a repo."""
 
     config = load_config()
     srcpkgdir = config[repo].base / 'src' / 'packages'
     for srpm in srcpkgdir.glob('**/*.src.rpm'):
         nvr = srpm.name.replace('.src.rpm', '')
-        click.echo(f'{nvr}')
+        name, _, _ = nvr.rsplit('-', maxsplit=2)
+        if not names or name in names:
+            click.echo(f'{nvr}')
 
 
 @click.command('repos')
