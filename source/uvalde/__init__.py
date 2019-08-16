5848861afd62813ee48392c5c972aea72952be28
Carl George <carl@george.computer>
2019-08-16 08:57:53 -0500

    Remove config command
    `uvalde config show` doesn't offer any advantage over just reading the
    config file.  The original intent was to add more config subcommands,
    but at this point I don't think that's likely.

--- source/uvalde/__init__.py
+++ source/uvalde/__init__.py
@@ -1,7 +1,6 @@
 import click
 
 from uvalde.import_command import import_
-from uvalde.config_commands import config_show
 from uvalde.list_command import list_
 from uvalde.move_command import move
 from uvalde.remove_command import remove
@@ -11,14 +10,6 @@ from uvalde.remove_command import remove
 def main():
     """Yum repository management tool."""
 
-
-@click.group('config')
-def config_group():
-    """Config subcommands."""
-
-
-config_group.add_command(config_show)
-main.add_command(config_group)
 
 main.add_command(import_)
 main.add_command(list_)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,27 +1,14 @@
-import textwrap
+import pathlib
 
 import click.testing
 
 import uvalde
 
 
-def test_config_show(tmp_path, tmp_config):
-    runner = click.testing.CliRunner()
-
-    result = runner.invoke(uvalde.main, ['config', 'show', 'repo1'])
-
-    assert result.output == textwrap.dedent('''\
-        [repo1]
-        base = {}/repo1
-        architectures = i686, x86_64
-    '''.format(tmp_path))
-    assert result.exit_code == 0
-
-
 def test_no_config(tmp_path):
     runner = click.testing.CliRunner()
 
-    result = runner.invoke(uvalde.main, ['config', 'show', 'repo0'])
+    result = runner.invoke(uvalde.main, ['list'])
 
     assert f'{tmp_path}/.config/uvalde/repos.ini: does not exist' in result.output
     assert result.exit_code == 1
@@ -30,7 +17,11 @@ def test_no_config(tmp_path):
 def test_no_repo(tmp_config):
     runner = click.testing.CliRunner()
 
-    result = runner.invoke(uvalde.main, ['config', 'show', 'repo0'])
+    args = ['import', '--keep', '--repo', 'repo0']
+    test_data = pathlib.Path('tests/data')
+    args.extend(map(str, test_data.glob('*.rpm')))
+
+    result = runner.invoke(uvalde.main, args)
 
     assert f'repo0: repo not configured' in result.output
     assert result.exit_code == 1
@@ -39,7 +30,7 @@ def test_no_repo(tmp_config):
 def test_missing_base(tmp_config_missing_base):
     runner = click.testing.CliRunner()
 
-    result = runner.invoke(uvalde.main, ['config', 'show', 'repo1'])
+    result = runner.invoke(uvalde.main, ['list'])
 
     assert f'repo1: missing parameter base' in result.output
     assert result.exit_code == 1
@@ -48,7 +39,11 @@ def test_missing_base(tmp_config_missing_base):
 def test_missing_architectures(tmp_config_missing_architectures):
     runner = click.testing.CliRunner()
 
-    result = runner.invoke(uvalde.main, ['config', 'show', 'repo1'])
+    args = ['import', '--keep', '--repo', 'repo1']
+    test_data = pathlib.Path('tests/data')
+    args.extend(map(str, test_data.glob('*.rpm')))
+
+    result = runner.invoke(uvalde.main, args)
 
     assert f'repo1: missing parameter architectures' in result.output
     assert result.exit_code == 1
